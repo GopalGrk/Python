ba0369e1910309ace587c7d3d0557f7866610fff
GopalGRK <gopalgrk.g@gmail.com>
2024-07-18 00:04:21 +0530

    new project

---
+++ jumping.py
@@ -0,0 +1,23 @@
+'''for i in range(1,4):
+    print(i)'''
+    
+'''for i in range(0,12,2):
+     print(i)'''
+
+
+'''a = 10
+while(a<0):
+    print(a)
+    a +=1'''
+
+'''for i in "pythonlife":
+    if i == 'l':
+        break
+    print(i)'''
+
+
+for i in "pythonlife":
+    if i == 'l':
+        continue
+    print(i)
+
